c9e7cd7ca4c65c8e36c3129379fb5957dcd58146
Pradnya Joshi <pradnyajoshi@Pradnyas-MBP.lan>
2022-07-16 17:32:40 -0400

    Change ball and bumpers to not be triggers anymore, switch player to trigger

--- GMTK2022/Assets/Resources/Scripts/Ball.cs
+++ GMTK2022/Assets/Resources/Scripts/Ball.cs
@@ -21,46 +21,67 @@ public class Ball : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         Bumper bumper = collision.gameObject.GetComponent<Bumper>();
-        if (bumper != null)
+        if (bumper != null || collision.gameObject.CompareTag("Wall"))
         {
-            // hit a bumper!
-            Debug.Log("Hit a bumper!");
-
-            Vector2 collisionPoint = collision.ClosestPoint(transform.position);
-            Vector2 collisionNormal = new Vector2(transform.position.x, transform.position.y) - collisionPoint;
-
-            if (bumper.IsCharged)
+            foreach (ContactPoint2D point in collision.contacts)
             {
-                rb.AddForce(bumper.BumpForce * collisionNormal.normalized, ForceMode2D.Impulse);
+                if (bumper != null && bumper.IsCharged)
+                {
+                    rb.velocity = Vector2.zero;
+                    rb.AddForce(bumper.BumpForce * point.normal.normalized, ForceMode2D.Impulse);
+                }
+                else
+                {
+                    rb.velocity = point.normal.normalized * rb.velocity.magnitude;  // keep current speed
+                }
+                break;
             }
-            else
-            {
-                rb.velocity = collisionNormal.normalized * Speed;
-            }
-
-            //// Below is the Jankies API....
-            //ContactPoint2D[] contactPoints = new ContactPoint2D[20];  // ??? wtf
-            //int contactPointsSize = collision.GetContacts(contactPoints);
-            //Debug.Log(contactPointsSize);
-
-            //// only look at first ContactPoint... for now
-            //for (int i = 0; i < contactPointsSize; i++)
-            //{
-            //    ContactPoint2D contactPoint = contactPoints[i];
-            //    rb.velocity = contactPoint.normal.normalized * Speed;
-            //    break;
-            //}
-        }
-        else if (collision.gameObject.CompareTag("Wall"))
-        {
-            Vector2 collisionPoint = collision.ClosestPoint(transform.position);
-            Vector2 collisionNormal = new Vector2(transform.position.x, transform.position.y) - collisionPoint;
-
-            // TODO: Change to AddForce possibly, adjust on game feel
-            rb.velocity = collisionNormal.normalized * Speed;
         }
     }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    Bumper bumper = collision.gameObject.GetComponent<Bumper>();
+    //    if (bumper != null)
+    //    {
+    //        // hit a bumper!
+    //        Debug.Log("Hit a bumper!");
+
+    //        Vector2 collisionPoint = collision.ClosestPoint(transform.position);
+    //        Vector2 collisionNormal = new Vector2(transform.position.x, transform.position.y) - collisionPoint;
+
+    //        if (bumper.IsCharged)
+    //        {
+    //            rb.AddForce(bumper.BumpForce * collisionNormal.normalized, ForceMode2D.Impulse);
+    //        }
+    //        else
+    //        {
+    //            rb.velocity = collisionNormal.normalized * Speed;
+    //        }
+
+    //        //// Below is the Jankies API....
+    //        //ContactPoint2D[] contactPoints = new ContactPoint2D[20];  // ??? wtf
+    //        //int contactPointsSize = collision.GetContacts(contactPoints);
+    //        //Debug.Log(contactPointsSize);
+
+    //        //// only look at first ContactPoint... for now
+    //        //for (int i = 0; i < contactPointsSize; i++)
+    //        //{
+    //        //    ContactPoint2D contactPoint = contactPoints[i];
+    //        //    rb.velocity = contactPoint.normal.normalized * Speed;
+    //        //    break;
+    //        //}
+    //    }
+    //    else if (collision.gameObject.CompareTag("Wall"))
+    //    {
+    //        Vector2 collisionPoint = collision.ClosestPoint(transform.position);
+    //        Vector2 collisionNormal = new Vector2(transform.position.x, transform.position.y) - collisionPoint;
+
+    //        // TODO: Change to AddForce possibly, adjust on game feel
+    //        rb.velocity = collisionNormal.normalized * Speed;
+    //    }
+    //}
 }
