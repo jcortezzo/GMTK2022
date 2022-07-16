dc730e0a53d6dc7a3fd68a4bc86c7712867dd5fa
Pradnya Joshi <pradnyajoshi@Pradnyas-MBP.lan>
2022-07-16 00:34:31 -0400

    Let player swing

--- GMTK2022/Assets/Resources/Scripts/Player.cs
+++ GMTK2022/Assets/Resources/Scripts/Player.cs
@@ -7,12 +7,17 @@ public class Player : MonoBehaviour
     [field:SerializeField]
     public float Speed { get; private set; }
 
+    [field:SerializeField]
+    public float SwingSpeed { get; private set; }
+
     [SerializeField]
     private GameObject BUMPER_PREFAB;
 
     private BumperZone bumperZone;
 
     private Rigidbody2D rb;
+    private Coroutine swingCoroutine;
+    private bool isSwinging = false;
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +39,7 @@ public class Player : MonoBehaviour
             }
         }
         if (n != 0) bumperZone.GenerateBumperFormation(n);
+        Swing();
         Move();
     }
 
@@ -42,6 +48,51 @@ public class Player : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector2 direction = new Vector2(horizontal, vertical).normalized;
-        rb.velocity = direction * Time.deltaTime * Speed;
+        rb.velocity = Speed * Time.deltaTime * direction;
+    }
+
+    void Swing()
+    {
+        bool swingLeft = Input.GetKeyDown(KeyCode.Q);
+        bool swingRight = Input.GetKeyDown(KeyCode.E);
+        if (swingLeft || swingRight)
+        {
+            if (swingCoroutine == null)
+            {
+                swingCoroutine = StartCoroutine(swingLeft ? RotateLeft(SwingSpeed) : RotateRight(SwingSpeed));
+            }
+            else if (!isSwinging)
+            {
+                StopCoroutine(swingCoroutine);
+                swingCoroutine = StartCoroutine(swingLeft ? RotateLeft(SwingSpeed) : RotateRight(SwingSpeed));
+            }
+        }
+    }
+
+    public IEnumerator RotateLeft(float duration)
+    {
+        isSwinging = true;
+        yield return Rotate(duration, 90);
+        isSwinging = false;
+    }
+
+    public IEnumerator RotateRight(float duration)
+    {
+        isSwinging = true;
+        yield return Rotate(duration, -90);
+        isSwinging = false;
+    }
+
+    private IEnumerator Rotate(float duration, float angle)
+    {
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = Quaternion.Euler(0, 0, angle) * startRotation;
+
+        for (float t = 0f; t < duration; t += Time.deltaTime / duration)
+        {
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, t / duration);
+            yield return null;
+        }
+        transform.rotation = endRotation;
     }
 }
